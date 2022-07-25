5da90d4c54051e49f64bac8b07765edc87676a6e
Chantal de Leste <chantal.delesteconde@plexus.es>
2022-07-25 12:15:13 +0200

    adding dependencies injector class file

---
+++ Marbel/Presentation/AppDependencies/AppDependencies.swift
@@ -0,0 +1,12 @@
+//
+//  AppDependencies.swift
+//  Marbel
+//
+//  Created by Chantal de Leste Conde on 25/7/22.
+//
+
+import Foundation
+
+class AppDependencies {
+
+}
--- Marbel/Presentation/Coordinators/MainCoordinator.swift
+++ Marbel/Presentation/Coordinators/MainCoordinator.swift
@@ -5,4 +5,35 @@
 //  Created by Chantal de Leste Conde on 25/7/22.
 //
 
-import Foundation
+import UIKit
+
+class MainCoordinator: NSObject, BaseCoordinator {
+    // MARK: - Properties
+    
+    let navigationController: UINavigationController
+    
+    private var appDependencies: AppDependencies
+    
+    // MARK: - Init
+    
+    public init(with navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        appDependencies = AppDependencies()
+    }
+    
+    func resolve() {
+        openSplashView()
+    }
+    
+    private func openSplashView() {
+        
+    }
+    
+    private func openMainListView() {
+        
+    }
+    
+    private func openListDetailView() {
+        
+    }
+}
