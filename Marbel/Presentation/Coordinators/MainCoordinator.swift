cd65a97c63572e470098f0b8e986dd1d7ab189af
Chantal de Leste <chantal.delesteconde@plexus.es>
2022-07-25 12:09:36 +0200

    Inital arquitecture and organization of this project

---
+++ Marbel/Presentation/Coordinators/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainCoordinator.swift
+//  Marbel
+//
+//  Created by Chantal de Leste Conde on 25/7/22.
+//
+
+import Foundation
---
+++ Marbel/Presentation/Core/BaseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  BaseCoordinator.swift
+//  Marbel
+//
+//  Created by Chantal de Leste Conde on 25/7/22.
+//
+
+import Foundation
